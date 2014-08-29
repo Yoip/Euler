1d205b5c9cde67dcabeb263d7583081c0c3040d8
Yoip <yoip1117@gmail.com>
2014-08-28 21:24:47 -0500

    Alex's PE 200

---
+++ Alex's200.py
@@ -0,0 +1,23 @@
+from Euler import *
+def sqube(a,b):
+  return a**2*b**3
+def isPrime(n):
+    if n==0:
+        return False
+    if n==1:
+        return False
+    if n==2:
+        return True
+    for i in range(2,int(n**.5)+1):
+        if n%i == 0:
+            return False
+    return True
+def isPrimeproof(n):
+    nums = []
+    for i in range(len(str(n))):
+        for a in range(10):
+            nums.append(int(str(n)[:i]+str(a)+str(n)[i+1:]))
+    for b in nums:
+        if isPrime(b):
+            return False
+    return True

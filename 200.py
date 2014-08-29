73721316fe5c0e3fa0ff73a3e45fcc6e3c2668eb
pwnd00d <austinpwnd00dzhou@gmail.com>
2014-08-28 20:49:47 -0500

    Update 200.py

--- 200.py
+++ 200.py
@@ -2,4 +2,12 @@ from Euler import *
 def sqube(a,b):
   return a**2*b**3
 def isPrimeproof(n):
+    nums = []
+    for i in range(len(str(n))):
+        for a in range(10):
+            nums.append(int(str(n)[:i]+str(a)+str(n)[i+1:]))
+    for b in nums:
+        if isPrime(b):
+            return False
+    return True
   
